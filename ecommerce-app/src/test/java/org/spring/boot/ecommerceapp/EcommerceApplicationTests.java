f4887b0954b66db39d0786b3a17061c333753d78
Adnan Rahin <adnan.rahin13@gmail.com>
2021-01-02 00:04:52 -0500

    First Commit

---
+++ ecommerce-app/src/test/java/org/spring/boot/ecommerceapp/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.spring.boot.ecommerceapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
