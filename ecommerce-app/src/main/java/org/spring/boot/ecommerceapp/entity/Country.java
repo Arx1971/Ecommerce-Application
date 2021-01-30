e70827c1858548db734d066d2b7b4af329dc8af0
Adnan Rahin <adnan.rahin13@gmail.com>
2021-01-30 16:15:16 -0500

    JPA mapping Done

---
+++ ecommerce-app/src/main/java/org/spring/boot/ecommerceapp/entity/Country.java
@@ -0,0 +1,55 @@
+package org.spring.boot.ecommerceapp.entity;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "country", schema = "full-stack-ecommerce")
+public class Country {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "code")
+    private String code;
+
+    @Column(name = "name")
+    private String name;
+
+    @OneToMany(mappedBy = "country")
+    private Set<State> states;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<State> getStates() {
+        return states;
+    }
+
+    public void setStates(Set<State> states) {
+        this.states = states;
+    }
+}
