0f8647ef0e7f52af2bb4365a6e446dbac1fc6fed
Adnan Rahin <adnan.rahin13@gmail.com>
2021-01-30 23:42:19 -0500

    Validators Added

--- ecommerce-frontend/src/app/components/checkout/checkout.component.ts
+++ ecommerce-frontend/src/app/components/checkout/checkout.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup} from '@angular/forms';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 import {OnlineShopFormService} from '../../services/online-shop-form.service';
 import {State} from '../../common/state';
 import {Country} from '../../common/country';
@@ -26,15 +26,18 @@ export class CheckoutComponent implements OnInit {
 
 
   constructor(private formBuilder: FormBuilder,
-              private onlineShopFormService: OnlineShopFormService) { }
+              private onlineShopFormService: OnlineShopFormService) {
+  }
 
   ngOnInit(): void {
 
     this.checkoutFormGroup = this.formBuilder.group({
       customer: this.formBuilder.group({
-        firstName: [''],
-        lastName: [''],
-        email: ['']
+        firstName: new FormControl('', [Validators.required, Validators.minLength(2)]),
+        lastName: new FormControl('', [Validators.required, Validators.minLength(2)]),
+        email: new FormControl('',
+          [Validators.required,
+            Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')])
       }),
       shippingAddress: this.formBuilder.group({
         street: [''],
@@ -91,8 +94,7 @@ export class CheckoutComponent implements OnInit {
       this.checkoutFormGroup.controls.billingAddress
         .setValue(this.checkoutFormGroup.controls.shippingAddress.value);
       this.billingAddressStates = this.shippingAddressStates;
-    }
-    else {
+    } else {
       this.checkoutFormGroup.controls.billingAddress.reset();
       this.billingAddressStates = [];
     }
@@ -124,8 +126,7 @@ export class CheckoutComponent implements OnInit {
 
     if (currentYear === selectedYear) {
       startMonth = new Date().getMonth() + 1;
-    }
-    else {
+    } else {
       startMonth = 1;
     }
 
@@ -152,8 +153,7 @@ export class CheckoutComponent implements OnInit {
       data => {
         if (formGroupName === 'shippingAddress') {
           this.shippingAddressStates = data;
-        }
-        else {
+        } else {
           this.billingAddressStates = data;
         }
         formGroup.get('state').setValue(data[0]);
