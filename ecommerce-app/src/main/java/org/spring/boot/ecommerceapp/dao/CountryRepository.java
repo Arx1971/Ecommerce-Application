a789ef7cb6de0d3065c5be9062c03e0e09bf8229
Adnan Rahin <adnan.rahin13@gmail.com>
2021-01-30 20:08:27 -0500

    All changes merge in master

---
+++ ecommerce-app/src/main/java/org/spring/boot/ecommerceapp/dao/CountryRepository.java
@@ -0,0 +1,12 @@
+package org.spring.boot.ecommerceapp.dao;
+
+
+import org.spring.boot.ecommerceapp.entity.Country;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin("http://localhost:4200")
+@RepositoryRestResource(collectionResourceRel = "countries", path = "countries")
+public interface CountryRepository extends JpaRepository<Country, Integer> {
+}
