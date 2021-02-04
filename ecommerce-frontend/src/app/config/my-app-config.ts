67357020ca9dfc711ecfa40430b8a544bb73cbb3
Adnan Rahin <adnan.rahin13@gmail.com>
2021-02-03 20:56:29 -0500

    new changes

---
+++ ecommerce-frontend/src/app/config/my-app-config.ts
@@ -0,0 +1,8 @@
+export default {
+  oidc: {
+    clientId: '0oa51x4ngzvKI8Or25d6',
+    issuer: 'https://dev-81860766.okta.com/oauth2/default',
+    redirectUri: 'http://localhost:4200/login/callback',
+    scopes: ['openid', 'profile', 'email']
+  }
+};
